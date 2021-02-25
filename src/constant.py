4ca2ee9964353d976180acb4a6296e781a0ceb6d
CJC-ds <carl.jason.chang@gmail.com>
2021-02-26 11:28:29 +1300

    added new words to STOPWORDS list, some ticker symbols are the same as some common words; picked up when posts are WRITTEN IN CAPSLOCK.

--- src/constant.py
+++ src/constant.py
@@ -2,5 +2,13 @@ API_QUERY_SIZE = 100
 NASDAQ = 'http://ftp.nasdaqtrader.com/dynamic/SymDir/nasdaqlisted.txt'
 OTHERS = 'http://ftp.nasdaqtrader.com/dynamic/SymDir/otherlisted.txt'
 STOPWORDS = [
-    'FOR', 'CEO', 'CFO'
+    'FOR'
+    , 'CEO'
+    , 'CFO'
+    , 'ALL'
+    , 'CAN'
+    , 'ARE'
+    , 'NEXT'
+    , 'NOW'
+    , 'BIG'
 ]
