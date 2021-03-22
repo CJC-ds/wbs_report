5d37ba40f5931fa2387d3957ac7ecdd8d730e34b
cjc-ds <carl.jason.chang@gmail.com>
2021-03-22 17:52:10 +1300

    includes the TIMEFRAME constant

--- src/constant.py
+++ src/constant.py
@@ -1,4 +1,5 @@
 API_QUERY_SIZE = 100
+TIMEFRAME = 7
 NASDAQ = 'http://ftp.nasdaqtrader.com/dynamic/SymDir/nasdaqlisted.txt'
 OTHERS = 'http://ftp.nasdaqtrader.com/dynamic/SymDir/otherlisted.txt'
 STOPWORDS = [
